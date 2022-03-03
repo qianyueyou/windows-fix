3536f5d06d034a3d4d3830f5fd2d567883ef757e
Qianyue You <36810244+qianyueyou@users.noreply.github.com>
2022-03-02 23:30:28 -0800

    ngen for powershell assemblies

--- new.install.ps1
+++ new.install.ps1
@@ -1,3 +1,16 @@
 # This disables the useless "troubleshooting" window when an application crashes.
 # https://docs.microsoft.com/en-us/powershell/module/windowserrorreporting/disable-windowserrorreporting?view=win10-ps
 Disable-WindowsErrorReporting
+
+# Use ngen to compile .NET assemblies to native images.  This should be run
+# after .NET framework updates.
+# See https://stackoverflow.com/questions/59341482/powershell-steps-to-fix-slow-startup
+$env:PATH = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+[AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object {
+    $path = $_.Location
+    if ($path) {
+        $name = Split-Path $path -Leaf
+        Write-Host -ForegroundColor Yellow "`r`nRunning ngen.exe on '$name'"
+        ngen.exe install $path /nologo
+    }
+}
