d14c698727527d0ac06385f8ec06dcc8acce3313
Qianyue You <36810244+qianyueyou@users.noreply.github.com>
2023-10-25 16:12:30 -0500

    Disable Memory Compression

--- new.install.ps1
+++ new.install.ps1
@@ -2,6 +2,10 @@
 # https://docs.microsoft.com/en-us/powershell/module/windowserrorreporting/disable-windowserrorreporting?view=win10-ps
 Disable-WindowsErrorReporting
 
+# Disable Memory Compression as it is broken on systems with large memory.
+# https://github.com/microsoft/Windows-Dev-Performance/issues/35
+Disable-MMAgent -mc
+
 # Use ngen to compile .NET assemblies to native images.  This should be run
 # after .NET framework updates.
 # See https://stackoverflow.com/questions/59341482/powershell-steps-to-fix-slow-startup
